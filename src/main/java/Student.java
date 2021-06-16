2e741388edea4ec130f4bbb837122ed994cc8141
Evan Williams <evan.b.williams95@gmail.com>
2021-06-16 09:09:10 -0500

    finished student-test

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,43 +1,61 @@
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Student {
 
-    private String name;
     private long id;
+    private String name;
+    private ArrayList<Integer> grades;
 
     public Student(String name, long id) {
         this.name = name;
         this.id = id;
+        this.grades = new ArrayList<>();
     }
 
-    public String getName() {
-        return name;
-    }
-
-
+    // returns the student's id
     public long getId() {
-        return id;
+        return this.id;
     }
 
-    public static ArrayList<Integer> grades =new ArrayList<Integer>();
+    // returns the student's name
+    public String getName() {
+        return this.name;
+    }
 
-
+    // adds the given grade to the grades list
     public void addGrade(int grade) {
-        if(grade>=0 && grade<=100){
-            grades.add(grade);
-        } else
-            System.out.println("Invalid grade.");
+        this.grades.add(grade);
+    }
+
+    // gets the given grade at the index
+    public int getGrade(int index) {
+        return this.grades.get(index);
+    }
+
+    // updates the given grade at the index
+    public void deleteGrade(int index) {
+        this.grades.remove(index);
+    }
+    // updates the given grade at the index
+    public void updateGrade(int index,int newGrade) {
+        this.grades.remove(index);
+        this.grades.add(index,newGrade);
+
     }
 
     // returns the list of grades
-    public ArrayList<Integer> getGrades(){
-        return grades;
+    public ArrayList<Integer> getGrades() {
+        return this.grades;
     }
 
     // returns the average of the students grades
-    public double getGradeAverage(){
-        return grades/grades.size();
+    public double getGradeAverage() {
+        double amount = this.grades.size();
+        double sum = 0;
+        for (int grade : grades) {
+            sum = sum + grade;
+        }
+        return sum / amount;
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,4 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
 public class StudentTest {
+
+    private Student testStudent;
+
+    @Before
+    public void studentTest(){
+        testStudent = new Student("Joe",1);
+        testStudent.addGrade(70);
+        testStudent.addGrade(71);
+    }
+
+    @Test
+    public void studentNameIDTest(){
+        int expectedID = 1;
+        String expectedName = "Joe";
+
+        assertEquals(expectedName,testStudent.getName());
+        assertEquals(expectedID,testStudent.getId());
+
+    }
+
+    @Test
+    public void studentGradesTest(){
+
+
+        ArrayList<Integer> expectedGrades = new ArrayList<>();
+        expectedGrades.add(70);
+        expectedGrades.add(71);
+
+        assertSame(expectedGrades.get(0), testStudent.getGrade(0));
+        assertSame(expectedGrades.get(1), testStudent.getGrade(1));
+
+
+    }
+
+
+    @Test
+    public void studentAverageTest(){
+
+        double expectedAverage = 70.5;
+        assertTrue(expectedAverage == testStudent.getGradeAverage());
+
+
+    }
+
+    @Test
+    public void studentGradeDelete(){
+        testStudent.deleteGrade(0);
+        assertEquals(71,testStudent.getGrade(0));
+        testStudent.addGrade(70);
+        //Array is now [71,70]
+    }
+
+    @Test
+    public void studentGradeUpdate(){
+        testStudent.updateGrade(1,80);
+        assertEquals(80,testStudent.getGrade(1));
+
+    }
+
 
 
 }
